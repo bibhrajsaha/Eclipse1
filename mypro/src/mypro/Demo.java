e5fed3052a892b45bccb114612beb9cf717bb5af
Saha, Bibhraj <saha.bibhraj@principal.com>
2022-03-09 12:26:05 +0530

    Java Project with jdk 1.4 has been created by Mr. Aaa.

---
+++ mypro/src/mypro/Demo.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package mypro;
+
+/**
+ * @author S606797
+ *
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List mylist = new ArrayList();
+		mylist.add(100);
+		mylist.add("Hello");
+		mylist.add(3.4);
+		System.out.println(mylist);
+
+	}
+
+}
